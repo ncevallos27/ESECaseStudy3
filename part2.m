e18dbba5738d48450f7a7e300f218d12c8c1c8c9
mindyyoung <mindyyoung@localhost>
2023-11-19 20:21:45 -0600

    still not done

--- part2.m
+++ part2.m
@@ -7,7 +7,7 @@ colormap("gray");
 % changing the sensor width :
 % changing the pixel :
 
-d3= 0.25;
+d3= 0.2;
 Md = [1 d3 0 0; 
       0 1 0 0; 
       0 0 1 d3; 
@@ -19,13 +19,30 @@ img_2 = rays2img(rays_2(1,:),rays_2(3,:),.5,5000);
 imagesc(img_2);
 colormap("gray");
 
-focal = .08;
+focal = .15;
 
 figure()
-M_f = [1 0 0 0; -1/focal 1 0 0; 0 0 1 0; 0 0 -1/focal 1];
+M_f = [1 0 0 0; 
+    -1/focal 1 0 0;
+    0 0 1 0; 
+    0 0 -1/focal 1];
+
 rays_3 = M_f * rays ; 
+
 img_3 = rays2img(rays_3(1,:),rays_3(3,:),.5, 3050);
 imagesc(img_3);
+colormap("gray");
+
+d3= 0.2;
+Md2 = [1 d3 0 0; 
+      0 1 0 0; 
+      0 0 1 d3; 
+      0 0 0 1];
+
+rays_4 = Md2 * rays_3 ; 
+
+img_4 = rays2img(rays_4(1,:),rays_4(3,:),.03, 3050);
+imagesc(img_4);
 colormap("gray");
 
 %not done
