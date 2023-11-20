be0409fbe8ff8fbced60c31d423c66c07471e905
mindyyoung <mindyyoung@localhost>
2023-11-19 23:44:57 -0600

    1

--- part2.m
+++ part2.m
@@ -19,7 +19,7 @@ img_2 = rays2img(rays_2(1,:),rays_2(3,:),.5,5000);
 imagesc(img_2);
 colormap("gray");
 
-focal = .15;
+    focal = .1377;
 
 figure()
 M_f = [1 0 0 0; 
@@ -33,15 +33,16 @@ img_3 = rays2img(rays_3(1,:),rays_3(3,:),.5, 3050);
 imagesc(img_3);
 colormap("gray");
 
-d3= 0.2;
-Md2 = [1 d3 0 0; 
+d4= 0.2;
+Md2 = [1 d4 0 0; 
       0 1 0 0; 
-      0 0 1 d3; 
+      0 0 1 d4; 
       0 0 0 1];
 
+figure()
 rays_4 = Md2 * rays_3 ; 
 
-img_4 = rays2img(rays_4(1,:),rays_4(3,:),.03, 3050);
+img_4 = rays2img(rays_4(1,:),rays_4(3,:),.05, 4000);
 imagesc(img_4);
 colormap("gray");
 
